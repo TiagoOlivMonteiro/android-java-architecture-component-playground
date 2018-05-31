41ad62cd5f6cb88e099ea054534f367c81908058
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-05-31 10:44:41 -0300

    Adds base structure

---
+++ app/src/main/java/me/tmonteiro/clashroyal/ui/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package me.tmonteiro.clashroyal.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.tmonteiro.clashroyal.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
