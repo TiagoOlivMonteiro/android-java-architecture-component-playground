67d86338c4636672a12da7a409df46fbda88798b
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-03 21:46:28 -0300

    Adds ClashRoyalApp

---
+++ app/src/main/java/me/tmonteiro/clashroyale/ClashRoyaleApp.java
@@ -0,0 +1,29 @@
+package me.tmonteiro.clashroyale;
+
+import android.app.Activity;
+import android.app.Application;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+import me.tmonteiro.clashroyale.di.AppInjector;
+
+public class ClashRoyalApp extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppInjector.init(this);
+    }
+
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
+}
