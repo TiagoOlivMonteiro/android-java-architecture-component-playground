e4ea142fb8d50c59b433c2ca1c443f139b06b98b
Tiago Monteiro <tiago.monteiro@fs.com.br>
2018-06-05 19:36:13 -0300

    Updates CardViewHolder

--- app/src/main/java/me/tmonteiro/clashroyale/ui/viewholder/CardViewHolder.java
+++ app/src/main/java/me/tmonteiro/clashroyale/ui/viewholder/CardViewHolder.java
@@ -2,6 +2,7 @@ package me.tmonteiro.clashroyale.ui.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import me.tmonteiro.clashroyale.R;
@@ -9,13 +10,19 @@ import me.tmonteiro.clashroyale.R;
 public class CardViewHolder extends RecyclerView.ViewHolder {
 
     final TextView name;
+    final ImageView icon;
 
     public CardViewHolder(View itemView) {
         super(itemView);
         name = itemView.findViewById(R.id.tv_name);
+        icon = itemView.findViewById(R.id.iv_icon);
     }
 
     public TextView getName() {
         return name;
+    }
+
+    public ImageView getIcon() {
+        return icon;
     }
 }
