a9a1719d5e9860d50a6d610aad869ab67ecf55af
Tiago Monteiro <tiago.monteiro@fs.com.br>
2018-06-06 18:58:14 -0300

    Updates CardDetailViewModel

--- app/src/main/java/me/tmonteiro/clashroyale/viewmodel/detail/CardDetailViewModel.java
+++ app/src/main/java/me/tmonteiro/clashroyale/viewmodel/detail/CardDetailViewModel.java
@@ -1,6 +1,36 @@
 package me.tmonteiro.clashroyale.viewmodel.detail;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import me.tmonteiro.clashroyale.repository.detail.CardDetailRepository;
+import me.tmonteiro.clashroyale.vo.detail.CardDetailComposition;
 
 public class CardDetailViewModel extends ViewModel {
+
+    private final CardDetailRepository cardDetailRepository;
+
+    public CardDetailViewModel(CardDetailRepository cardDetailRepository) {
+        this.cardDetailRepository = cardDetailRepository;
+    }
+
+    public LiveData<CardDetailComposition> getCard(String idName) {
+        return this.cardDetailRepository.getCard(idName);
+    }
+
+    public static class Factory implements ViewModelProvider.Factory {
+
+        private final CardDetailRepository cardDetailRepository;
+
+        public Factory(CardDetailRepository cardDetailRepository) {
+            this.cardDetailRepository = cardDetailRepository;
+        }
+
+        @Override
+        public CardDetailViewModel create(Class modelClass) {
+            return new CardDetailViewModel(cardDetailRepository);
+        }
+
+    }
 }
