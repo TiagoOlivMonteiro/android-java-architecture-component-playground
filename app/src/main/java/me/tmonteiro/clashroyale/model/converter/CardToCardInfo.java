b7f8f50002f37ba3b3a8faf79c16f3f0f4e8500b
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-05 23:42:28 -0300

    Adds /model/converter/

---
+++ app/src/main/java/me/tmonteiro/clashroyale/model/converter/CardToCardInfo.java
@@ -0,0 +1,25 @@
+package me.tmonteiro.clashroyale.model.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.tmonteiro.clashroyale.model.card.Card;
+import me.tmonteiro.clashroyale.vo.card.CardInfo;
+
+public class CardToCardInfo {
+
+    public static List<CardInfo> convertCardListToCardInfoList(List<Card> list) {
+        List<CardInfo> ret = new ArrayList<>();
+        CardInfo cardInfo;
+
+        for (Card card : list) {
+            cardInfo = new CardInfo();
+            cardInfo.setName(card.getName());
+            cardInfo.setIconUrl(card.getImageUrl());
+            cardInfo.setIdName(card.getIdName());
+            ret.add(cardInfo);
+        }
+
+        return ret;
+    }
+}
