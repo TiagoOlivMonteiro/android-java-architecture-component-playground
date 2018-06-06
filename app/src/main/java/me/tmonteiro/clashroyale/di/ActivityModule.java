5e2ca08fd8ea7b0363ac98542ac5b144917d4319
Tiago Monteiro <tiago.monteiro@fs.com.br>
2018-06-06 18:56:27 -0300

    Updates ACtivityModule

--- app/src/main/java/me/tmonteiro/clashroyale/di/ActivityModule.java
+++ app/src/main/java/me/tmonteiro/clashroyale/di/ActivityModule.java
@@ -2,11 +2,15 @@ package me.tmonteiro.clashroyale.di;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
+import me.tmonteiro.clashroyale.ui.activity.DetailActivity;
 import me.tmonteiro.clashroyale.ui.activity.HomeActivity;
 
 @Module
 public abstract class ActivityModule {
 
     @ContributesAndroidInjector(modules = {HomeModule.class})
-    abstract HomeActivity contributeActivityInjector();
+    abstract HomeActivity contributeHomeActivityInjector();
+
+    @ContributesAndroidInjector(modules = {DetailModule.class})
+    abstract DetailActivity contributeDetailActivityInjector();
 }
