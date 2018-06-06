9cb014fa956f1028fee1b78219f1000c4737e4e2
Tiago Monteiro <tiago.monteiro@fs.com.br>
2018-06-06 18:56:10 -0300

    Updates ClashRoyaleApp

--- app/src/main/java/me/tmonteiro/clashroyale/ClashRoyaleApp.java
+++ app/src/main/java/me/tmonteiro/clashroyale/ClashRoyaleApp.java
@@ -10,6 +10,10 @@ import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
 import me.tmonteiro.clashroyale.di.AppInjector;
 
+// TODO: Design
+// TODO: API: Loading, Error
+// TODO: Testing: Instrumented teste and unit test
+// TODO: CI
 public class ClashRoyaleApp extends Application implements HasActivityInjector {
 
     @Inject
