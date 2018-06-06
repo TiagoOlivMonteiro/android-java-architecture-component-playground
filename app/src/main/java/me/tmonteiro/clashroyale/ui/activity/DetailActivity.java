d659dceb17754d6f2953a11445fa0d3d841131cc
Tiago Monteiro <tiago.monteiro@fs.com.br>
2018-06-06 18:55:40 -0300

    Adds DetailActivity

---
+++ app/src/main/java/me/tmonteiro/clashroyale/ui/activity/DetailActivity.java
@@ -0,0 +1,72 @@
+package me.tmonteiro.clashroyale.ui.activity;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import javax.inject.Inject;
+
+import me.tmonteiro.clashroyale.R;
+import me.tmonteiro.clashroyale.di.Injectable;
+import me.tmonteiro.clashroyale.viewmodel.detail.CardDetailViewModel;
+import me.tmonteiro.clashroyale.vo.detail.CardDetailComposition;
+import me.tmonteiro.clashroyale.vo.detail.CardDetailInfo;
+
+public class DetailActivity extends AppCompatActivity implements Injectable {
+
+    public static final String EXTRA_ID_NAME = "idName";
+
+    @Inject
+    ViewModelProvider.Factory factory;
+
+    private String idName;
+    private CardDetailViewModel cardDetailViewModel;
+    private TextView tvDescription;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        handleExtra();
+        setupViewModel();
+        setupView();
+        loadCard();
+    }
+
+    private void setupView() {
+        this.tvDescription = findViewById(R.id.tv_description);
+    }
+
+    private void handleExtra() {
+        final Bundle bundle = getIntent().getExtras();
+        this.idName = bundle.getString(EXTRA_ID_NAME);
+    }
+
+    private void setupViewModel() {
+        this.cardDetailViewModel = ViewModelProviders.of(this, factory)
+                .get(CardDetailViewModel.class);
+    }
+
+    private void loadCard() {
+        this.cardDetailViewModel.getCard(this.idName).observe(this, new Observer<CardDetailComposition>() {
+            @Override
+            public void onChanged(@Nullable CardDetailComposition cardDetailComposition) {
+                switch (cardDetailComposition.getStatus()) {
+                    case SUCCESS:
+                        setCardInfo(cardDetailComposition.getResult());
+                        break;
+                }
+            }
+        });
+    }
+
+    private void setCardInfo(CardDetailInfo cardDetailInfo) {
+        this.tvDescription.setText(cardDetailInfo.getDescription());
+    }
+
+
+}
