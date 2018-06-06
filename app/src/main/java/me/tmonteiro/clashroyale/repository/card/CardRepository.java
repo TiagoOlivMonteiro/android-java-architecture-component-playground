4f347a4a1beb13c667d639df0e938e10f839b2d0
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-05 23:43:21 -0300

    Refactors CardRepository

--- app/src/main/java/me/tmonteiro/clashroyale/repository/card/CardRepository.java
+++ app/src/main/java/me/tmonteiro/clashroyale/repository/card/CardRepository.java
@@ -3,15 +3,14 @@ package me.tmonteiro.clashroyale.repository.card;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import me.tmonteiro.clashroyale.api.CardAPI;
 import me.tmonteiro.clashroyale.model.card.Card;
+import me.tmonteiro.clashroyale.model.converter.CardToCardInfo;
 import me.tmonteiro.clashroyale.vo.card.CardComposition;
-import me.tmonteiro.clashroyale.vo.card.CardInfo;
 import me.tmonteiro.clashroyale.vo.card.CardStatus;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -49,30 +48,20 @@ public class CardRepository {
             @Override
             public void onResponse(Call<List<Card>> call, Response<List<Card>> response) {
                 cardHolder.setStatus(CardStatus.SUCCESS);
-                cardHolder.setResult(convertCardListToCardInfoList(response.body()));
+                cardHolder.setResult(CardToCardInfo.convertCardListToCardInfoList(response.body()));
+                listCard.setValue(cardHolder);
             }
 
             @Override
             public void onFailure(Call<List<Card>> call, Throwable t) {
                 cardHolder.setStatus(CardStatus.ERROR);
+                listCard.setValue(null);
             }
         });
 
         listCard.postValue(cardHolder);
     }
 
-    private static List<CardInfo> convertCardListToCardInfoList(List<Card> list) {
-        List<CardInfo> ret = new ArrayList<>();
-        CardInfo cardInfo;
 
-        for (Card card : list) {
-            cardInfo = new CardInfo();
-            cardInfo.setName(card.getName());
-            cardInfo.setIconUrl(card.getImageUrl());
-            ret.add(cardInfo);
-        }
-
-        return ret;
-    }
 
 }
