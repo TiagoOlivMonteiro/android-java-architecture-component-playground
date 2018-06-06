40fc064225d1be159fe35e9e86f2dc91f8862dc3
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-05 23:44:04 -0300

    Updates CardAdapter adding click listener

--- app/src/main/java/me/tmonteiro/clashroyale/ui/adapter/CardAdapter.java
+++ app/src/main/java/me/tmonteiro/clashroyale/ui/adapter/CardAdapter.java
@@ -12,16 +12,19 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 import me.tmonteiro.clashroyale.R;
-import me.tmonteiro.clashroyale.vo.card.CardInfo;
 import me.tmonteiro.clashroyale.ui.viewholder.CardViewHolder;
+import me.tmonteiro.clashroyale.vo.card.CardInfo;
 
-public class CardAdapter extends RecyclerView.Adapter  {
+public class CardAdapter extends RecyclerView.Adapter {
 
-    List<CardInfo> cardInfoList;
-    Context context;
+    private final Context context;
+    private final CardAdapterItemSelected cardAdapterItemSelected;
+    private List<CardInfo> cardInfoList;
 
-    public CardAdapter(Context context, List<CardInfo> cardInfoModelList) {
+
+    public CardAdapter(Context context, CardAdapterItemSelected cardAdapterItemSelected, List<CardInfo> cardInfoModelList) {
         this.context = context;
+        this.cardAdapterItemSelected = cardAdapterItemSelected;
         this.cardInfoList = cardInfoModelList;
     }
 
@@ -30,7 +33,18 @@ public class CardAdapter extends RecyclerView.Adapter  {
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context)
                 .inflate(R.layout.item_home, parent, false);
-        CardViewHolder holder = new CardViewHolder(view);
+        final CardViewHolder holder = new CardViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int itemPosition = holder.getAdapterPosition();
+                final CardInfo cardInfo = cardInfoList.get(itemPosition);
+                if(cardInfo != null){
+                    cardAdapterItemSelected.itemSelected(cardInfo.getName());
+                }
+
+            }
+        });
         return holder;
     }
 
@@ -44,6 +58,9 @@ public class CardAdapter extends RecyclerView.Adapter  {
 
     @Override
     public int getItemCount() {
-        return cardInfoList.size();
+        return cardInfoList == null ? 0 : cardInfoList.size();
     }
+
+
+
 }
