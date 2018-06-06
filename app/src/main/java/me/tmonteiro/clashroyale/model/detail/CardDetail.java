b7c205c9fedd5f40b935abbd8a51c289dab180b8
Tiago Monteiro <tiago.monteiro@fs.com.br>
2018-06-06 18:54:45 -0300

    Adds /model/detail/

---
+++ app/src/main/java/me/tmonteiro/clashroyale/model/detail/CardDetail.java
@@ -0,0 +1,97 @@
+package me.tmonteiro.clashroyale.model.detail;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class CardDetail {
+
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("idName")
+    @Expose
+    private String idName;
+    @SerializedName("rarity")
+    @Expose
+    private String rarity;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("elixirCost")
+    @Expose
+    private Integer elixirCost;
+    @SerializedName("arena")
+    @Expose
+    private Integer arena;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getIdName() {
+        return idName;
+    }
+
+    public void setIdName(String idName) {
+        this.idName = idName;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getElixirCost() {
+        return elixirCost;
+    }
+
+    public void setElixirCost(Integer elixirCost) {
+        this.elixirCost = elixirCost;
+    }
+
+    public Integer getArena() {
+        return arena;
+    }
+
+    public void setArena(Integer arena) {
+        this.arena = arena;
+    }
+}
