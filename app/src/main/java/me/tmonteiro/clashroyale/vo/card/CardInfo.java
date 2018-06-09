88042708ef02da7f4a5d6e000e575052d1b0acc3
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-09 12:26:33 -0300

    Updates CardInfo

--- app/src/main/java/me/tmonteiro/clashroyale/vo/card/CardInfo.java
+++ app/src/main/java/me/tmonteiro/clashroyale/vo/card/CardInfo.java
@@ -5,6 +5,9 @@ public class CardInfo {
     private String idName;
     private String name;
     private String iconUrl;
+    private int elixirCost;
+    private String rarity;
+    private int arena;
 
     public String getIdName() {
         return idName;
@@ -28,5 +31,29 @@ public class CardInfo {
 
     public void setIconUrl(String iconUrl) {
         this.iconUrl = iconUrl;
+    }
+
+    public Integer getElixirCost() {
+        return elixirCost;
+    }
+
+    public void setElixirCost(int elixirCost) {
+        this.elixirCost = elixirCost;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public Integer getArena() {
+        return arena;
+    }
+
+    public void setArena(int arena) {
+        this.arena = arena;
     }
 }
