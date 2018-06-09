81fcae9aa722f7fe56c98f7322ee0302d14fdd77
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-09 12:25:42 -0300

    Updates CardAdapter

--- app/src/main/java/me/tmonteiro/clashroyale/ui/adapter/CardAdapter.java
+++ app/src/main/java/me/tmonteiro/clashroyale/ui/adapter/CardAdapter.java
@@ -1,6 +1,7 @@
 package me.tmonteiro.clashroyale.ui.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -21,6 +22,10 @@ public class CardAdapter extends RecyclerView.Adapter {
     private final CardAdapterItemSelected cardAdapterItemSelected;
     private List<CardInfo> cardInfoList;
 
+    private final static String CARD_RARITY_COMMON = "Common";
+    private final static String CARD_RARITY_RARE = "Rare";
+    private final static String CARD_RARITY_EPIC = "Epic";
+    private final static String CARD_RARITY_LEGENDARY = "Legendary";
 
     public CardAdapter(Context context, CardAdapterItemSelected cardAdapterItemSelected, List<CardInfo> cardInfoModelList) {
         this.context = context;
@@ -39,7 +44,7 @@ public class CardAdapter extends RecyclerView.Adapter {
             public void onClick(View view) {
                 final int itemPosition = holder.getAdapterPosition();
                 final CardInfo cardInfo = cardInfoList.get(itemPosition);
-                if(cardInfo != null){
+                if (cardInfo != null) {
                     cardAdapterItemSelected.itemSelected(cardInfo.getIdName());
                 }
 
@@ -52,15 +57,44 @@ public class CardAdapter extends RecyclerView.Adapter {
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
         CardViewHolder holder = (CardViewHolder) viewHolder;
         CardInfo cardInfo = cardInfoList.get(position);
+        handleCardRarity(holder, cardInfo);
         holder.getName().setText(cardInfo.getName());
+        holder.getElixirCost().setText(String.format(context.getString(R.string.card_elixir_cost), cardInfo.getElixirCost()));
+        holder.getArena().setText(String.format(context.getString(R.string.card_arena), cardInfo.getArena()));
         Picasso.get().load(cardInfo.getIconUrl()).into(holder.getIcon());
+    }
+
+    private void handleCardRarity(CardViewHolder holder, CardInfo cardInfo) {
+
+        final View viewRarity = holder.getRarity();
+
+        switch (cardInfo.getRarity()) {
+            case CARD_RARITY_COMMON:
+                viewRarity.setBackground(context.getResources().getDrawable(R.drawable.card_common));
+                break;
+            case CARD_RARITY_RARE:
+                viewRarity.setBackground(context.getResources().getDrawable(R.drawable.card_rare));
+                break;
+            case CARD_RARITY_EPIC:
+                viewRarity.setBackground(context.getResources().getDrawable(R.drawable.card_epic));
+                break;
+            case CARD_RARITY_LEGENDARY:
+                viewRarity.setBackground(context.getResources().getDrawable(R.drawable.card_legendary));
+                final AnimationDrawable animationDrawable = (AnimationDrawable) viewRarity.getBackground();
+                animationDrawable.setEnterFadeDuration(2000);
+                animationDrawable.setExitFadeDuration(2000);
+                animationDrawable.start();
+                break;
+            default:
+                viewRarity.setBackground(context.getResources().getDrawable(R.drawable.card_common));
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
         return cardInfoList == null ? 0 : cardInfoList.size();
     }
-
 
 
 }
