a169c50c14d4b178c37e8f1501cffb4217169096
Tiago Monteiro <tiago.oliveira.monteiro@gmail.com>
2018-06-09 12:26:08 -0300

    Updates CardViewHolder

--- app/src/main/java/me/tmonteiro/clashroyale/ui/viewholder/CardViewHolder.java
+++ app/src/main/java/me/tmonteiro/clashroyale/ui/viewholder/CardViewHolder.java
@@ -11,11 +11,17 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
 
     final TextView name;
     final ImageView icon;
+    final View rarity;
+    final TextView elixirCost;
+    final TextView arena;
 
     public CardViewHolder(View itemView) {
         super(itemView);
         name = itemView.findViewById(R.id.tv_name);
         icon = itemView.findViewById(R.id.iv_icon);
+        rarity = itemView.findViewById(R.id.view_rarity);
+        elixirCost = itemView.findViewById(R.id.tv_elixir_cost);
+        arena = itemView.findViewById(R.id.tv_arena);
     }
 
     public TextView getName() {
@@ -24,5 +30,17 @@ public class CardViewHolder extends RecyclerView.ViewHolder {
 
     public ImageView getIcon() {
         return icon;
+    }
+
+    public View getRarity() {
+        return rarity;
+    }
+
+    public TextView getElixirCost() {
+        return elixirCost;
+    }
+
+    public TextView getArena() {
+        return arena;
     }
 }
